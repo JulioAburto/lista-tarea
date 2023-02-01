2d3b71818113371ee21e779aae3e769397a47a77
Julio <juliocesaraburtoguido@gmail.com>
2023-01-31 22:30:37 -0600

    Añadiendo proyecto de tareas con la opcion de eliminar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import logo from "./img/logo.png";
+import ListaDeTarea from "./component/ListaDeTareas";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="logo-contenedor">
+				<img src={logo} className="logo" alt="logo"></img>
+			</div>
+			<div className="tareaLista">
+				<h1> Mis tareas</h1>
+				<ListaDeTarea></ListaDeTarea>
+			</div>
+		</div>
+	);
 }
 
 export default App;
